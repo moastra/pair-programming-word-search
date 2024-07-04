602fc8e8fe2f2f48e9bd2a48c6dabd0e831190f4
Mathew M <mathewmastromonaco@gmail.com>
2024-07-03 23:02:50 -0400

    fixed buggy code, added vertical array creation and vertical check.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+if (letters.length === 0) return false;
 
-module.exports = wordSearch
+    const horizontalJoin = letters.map(ls => ls.join('')) // horizontal check
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    } 
+    const verticalJoin = []; // vertical array creation
+    for (let vert = 0; vert < letters[0].length; vert++) {
+        let verticalWord = '';
+        for (let hor = 0; hor < letters.length; hor++) {
+            verticalWord += letters[hor][vert];
+        }
+        verticalJoin.push(verticalWord);
+    }
+    for (const v of verticalJoin) { // vertical check
+        if (v.includes(word)) return true;
+    }
+
+    return false;
+};
+
+
+module.exports = wordSearch;
